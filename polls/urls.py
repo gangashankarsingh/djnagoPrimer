4ce15dcb1eeef6405b51a39df9bf2c63218f797b
Gangashankar <bigdatamonk@gmail.com>
2019-12-11 00:25:06 +0530

    Created different views for different URL patterns

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
+    path('<int:id>', views.detail),
+    path('<int:id>/results', views.result),
+    path('<int:id>/vote', views.voting),
 
 ]
